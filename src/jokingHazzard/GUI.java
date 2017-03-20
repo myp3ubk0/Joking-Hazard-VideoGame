8d42938c16516dbb780148bc4542804a51cb41da
myp3ubk0 <myp3ubk0@gmail.com>
2017-03-21 04:23:15 +0500

    Make an GUI test
    there comes gui in center of screen

--- src/jokingHazzard/GUI.java
+++ src/jokingHazzard/GUI.java
@@ -1,9 +1,19 @@
 package jokingHazzard;
 
+import javax.swing.JFrame;
+
+import com.sun.glass.ui.Screen;
+
 public class GUI {
 
 	public static void main(String[] args) {
-		System.out.print("123");
+		JFrame.setDefaultLookAndFeelDecorated(false); //  устанавливае стандартный внешний вид
+        JFrame frame = new JFrame("Здесь заголовок окна"); // желаемый заголовок        
+        // задаём операцию, которая выполниться при закрытии
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300); // задаём размер окна 
+        frame.setVisible(true); //  форма будет видимой
+        frame.setLocationRelativeTo(null);
 	}
 
 }
